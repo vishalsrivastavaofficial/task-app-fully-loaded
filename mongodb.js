49857f093fc819d9098e4f637be4ea285b732057
Vishal Srivastava <vishalsrivastava.official@gmail.com>
2020-05-10 01:34:21 +0530

    mongoose api

--- mongodb.js
+++ mongodb.js
@@ -4,20 +4,20 @@
 // const MongoClient = mongodb.MongoClient
 // const ObjectID = mongodb.ObjectID
 
-const { MongoClient, ObjectID } = require('mongodb')
+// const { MongoClient, ObjectID } = require('mongodb')
 
-const connectionURL = 'mongodb://127.0.0.1:27017'
-const databaseName = 'task-manager'
+// const connectionURL = 'mongodb://127.0.0.1:27017'
+// const databaseName = 'task-manager'
 
 // const id = new ObjectID()
 // console.log(id.getTimestamp())
 
-MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
-    if (error) {
-        return console.log('Unable to connect to db!')
-    }
+// MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
+//     if (error) {
+//         return console.log('Unable to connect to db!')
+//     }
 
-    const db = client.db(databaseName)
+//     const db = client.db(databaseName)
     // db.collection('users').insertOne({
     //     name: 'Vishal Srivastava',
     //     age: 27
@@ -57,13 +57,48 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: 
     //     console.log(result.ops)
     // })
 
-    db.collection('users').findOne({ age: 18 }, (error, result)=> {
-        if(error) {
-            return console.log('Unable to find user')
-        }
+    // db.collection('users').findOne({ age: 18 }, (error, result)=> {
+    //     if(error) {
+    //         return console.log('Unable to find user')
+    //     }
 
-        console.log(result)
-    })
+    //     console.log(result)
+    // })
 
-})
+    // db.collection('users').find({ age: 18 }).toArray((error, result)=> {
+    //     if(error) {
+    //         return console.log('Unable to find user')
+    //     }
+
+    //     console.log(result)
+    // })
+
+    // db.collection('users').find({ age: 18 }).count((error, result)=> {
+    //     if(error) {
+    //         return console.log('Unable to find user')
+    //     }
+
+    //     console.log(result)
+    // })
+
+    // db.collection('users').updateOne({
+    //     _id: new ObjectID('5eb5b5b7b7473e3684879047')
+    // }, {
+    //     $inc: {
+    //         age: 1
+    //     }
+    // }).then((result)=> {
+    //     console.log(result)
+    // }).catch((error)=> {
+    //     console.log(error)
+    // })
+
+//     db.collection('users').deleteMany({ age:27 }
+//         ).then((result)=> {
+//         console.log(result)
+//     }).catch((error)=> {
+//         console.log(error)
+//     })
+
+// })
 
---
+++ src/models/user.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7,
+        trim: true,
+        validate(value) {
+            if(value.toLowerCase().includes('password')) {
+                throw new Error('Password can not be password')
+            }
+        }
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if(value < 0) {
+                throw new Error('Age must be a positive number')
+            }
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if(!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
+
+    }
+})
+
+module.exports = User

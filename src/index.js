3a2a5a24b031f70bcffdc35e1009f9a61245346c
Vishal Srivastava <vishalsrivastava.official@gmail.com>
2020-05-13 15:35:34 +0530

    task api await included

--- src/index.js
+++ src/index.js
@@ -90,34 +90,84 @@ app.patch('/users/:id', async (req, res) => {
     }
 })
 
-app.post('/tasks', (req, res)=>{
+app.post('/tasks',async (req, res)=>{
     const task = new Task(req.body)
-    task.save().then((result) => {
+
+    try{
+        await task.save()
         res.status(201).send(task)
-    }).catch((error) => {
+    } catch(e) {
         res.status(400)
         .send(error)
-    })
+    }
+    // task.save().then((result) => {
+    //     res.status(201).send(task)
+    // }).catch((error) => {
+    //     res.status(400)
+    //     .send(error)
+    // })
 })
 
-app.get('/tasks', (req, res)=> {
-    Task.find({}).then((task)=>{
-        res.send(task)
-    }).catch((e)=>{
-        res.status(500).send()
-    })
+app.get('/tasks', async (req, res)=> {
+
+    try{
+        const tasks = await Task.find({})
+        res.send(tasks)
+    }catch(e){
+        res.send(500).send()
+    }
+
+    // Task.find({}).then((task)=>{
+    //     res.send(task)
+    // }).catch((e)=>{
+    //     res.status(500).send()
+    // })
 })
 
-app.get('/tasks/:id', (req, res)=> {
+app.get('/tasks/:id',async (req, res)=> {
     const _id = req.params.id 
-    Task.findById(_id).then((task)=>{
+
+    try{
+        const task = await Task.findById(_id)
         if(!task) {
             return res.status(404).send()
         }
         res.send(task)
-    }).catch((e)=>{
+    }catch(e){
         res.status(500).send()
+    }
+
+    // Task.findById(_id).then((task)=>{
+    //     if(!task) {
+    //         return res.status(404).send()
+    //     }
+    //     res.send(task)
+    // }).catch((e)=>{
+    //     res.status(500).send()
+    // })
+})
+
+app.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description', 'completed']
+    const isValidOperation = updates.every((update)=>{
+        return allowedUpdates.includes(updates)
     })
+
+    if(!isValidOperation){
+        return res.status(400).send({error: 'Invalid updates'})
+    }
+    const _id = req.params.id 
+    try{
+        const task = await User.findByIdAndUpdate(_id, 
+            req.body, { new: true, runValidators: true })
+        if(!task) {
+            return res.status(404).send()
+        }
+        res.send(task)
+    } catch(e){
+        res.status(400).send(e)
+    }
 })
 
 app.listen(port, ()=> {
